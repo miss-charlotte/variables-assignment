d41430137b9ba91c55c647e4a2bd40eee72791eb
miss-jagger <charlotte.akongo@gmail.com>
2023-07-18 19:50:54 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    // program that calculates my age
+    val yearOfBirth = 1998
+    val currentYear = 2023
+    val myAge = currentYear - yearOfBirth
+    println("My age is $myAge")
+
+    //if a right-angled triangle has base 4 and height 3, find the hypotenuse
+    val b = 4
+    val c = 3
+    val a = (b * b) + (c * c)
+    print("$a")
+
+}
